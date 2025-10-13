a713bf3ba9d8c7a45ce44fa03f6397b63db2c4ad
Khang Kyle Huynh <kkyhuynh@gmail.com>
2025-10-13 14:23:08 -0500

    first test

---
+++ CICD Demo.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit; 
+
+namespace CICD Demo.Tests
+{
+    public class UnitTest1 
+    {
+        public void Addition_ShouldWork()
+        {
+            int result = 2 + 2;
+            Assert.Equal(4, result); 
+        }
+    }
+}
+
